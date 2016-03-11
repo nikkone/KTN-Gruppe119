2bc1e00628330b172070a52285e338b2e208212d
nikkone <nikolal@stud.ntnu.no>
2016-03-11 16:37:08 +0100

    Nesten resten
    Lagt til alt utenom sjekking av brukernavn

--- Server/Server.py
+++ Server/Server.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 import SocketServer
 import json
-
+import time
 """
 Variables and functions that must be used by all the ClientHandler objects
 must be written here (e.g. a dictionary for connected clients)
 """
 
 users = {}
-#connections = []
-#history = []
+history = []
 
 class ClientHandler(SocketServer.BaseRequestHandler):
     """
@@ -18,7 +17,14 @@ class ClientHandler(SocketServer.BaseRequestHandler):
     only connected clients, and not the server itself. If you want to write
     logic for the server, you must write it outside this class
     """
+    def sendToSelf(self, timestamp, sender, response, content):
+        json_message = json.dumps({'timestamp': timestamp, 'sender': sender, 'response': response, 'content': content})
+        self.connection.send(json_message)
 
+    def sendToAll(self, timestamp, sender, response, content):
+        for key in users:
+            users[key].sendToSelf(timestamp, sender, response, content)
+    
     def handle(self):
         """
         This method handles the connection between a client and the server.
@@ -34,38 +40,58 @@ class ClientHandler(SocketServer.BaseRequestHandler):
                 received_json = json.loads(received_string)
                 content = received_json["content"]
                 request = received_json["request"]
-                #print(request)
-                #print(content)
+                
                 if(self.username!=''):
                     if(request=="msg"):
                         print("Message received")
+                        self.sendToAll(time.strftime("%H:%M:%S"), self.username, "message", content)
+                        history.append((self.username, content))
+                    elif(request=="names"):
+                        print("Names requested")
+                        namesString="Connected users: "
+                        for key in users:
+                            namesString+=key + " - "
+                        self.sendToSelf(time.strftime("%H:%M:%S"), "server", "info", namesString)
+                    elif(request=="history"):
+                        print("History requested")
+                        historyString=""
+                        for msg in history:
+                            historyString += msg[1] + '\n'
+                        self.sendToSelf(time.strftime("%H:%M:%S"), "server", "history", historyString)
+                    elif(request=="help"):
+                        print("Help requested")
+                        self.sendToSelf(time.strftime("%H:%M:%S"), "server", "info", "Hjelp er for pingler!")
                     elif(request=="login"):
                         print("ERROR: Already logged in")
+                        self.sendToSelf(time.strftime("%H:%M:%S"), "server", "error", "Already logged in")
                     elif(request=="logout"):
                         print("User removed:" + self.username)
+                        self.sendToSelf(time.strftime("%H:%M:%S"), "server", "info", "Logout sucessfull")
                         users.pop(self.username, None)
                         self.username=''
                     else:
                         print("ERROR: Unknown request")
-                    
-                    print(users)
+                        self.sendToSelf(time.strftime("%H:%M:%S"), "server", "eror", "Unknown request")  
                 else:
                     if(request=="login"):
                         if(content in users):
                             print("ERROR: Username taken")
+                            self.sendToSelf(time.strftime("%H:%M:%S"), "server", "error", "Username taken")
                         elif(content==''):
                             print("ERROR: No username given")
+                            self.sendToSelf(time.strftime("%H:%M:%S"), "server", "error", "No username given")
                         else:
                             print("User added: " + content)
+                            self.sendToSelf(time.strftime("%H:%M:%S"), "server", "info", "Login sucessfull")
                             self.username=content
                             users[content] = self
                     else:
                         print("ERROR: Not logged in")
+                        self.sendToSelf(time.strftime("%H:%M:%S"), "server", "error", "Not logged in")
                         
             except ValueError:
                 print("Not JSON-Object, closing.")
                 self.connection.close()
-            # TODO: Add handling of received payload from client
 
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
